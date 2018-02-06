23333e28ee7d330ba3e460f3a85ca480af2d2026
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-06 14:12:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,8 +15,8 @@ class Student
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    row = DB[:conn].execute("SELECT * FROM students WHERE name=?", name)
+    self.new_from_db(row)
   end
 
   def save
