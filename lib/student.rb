3c7b2b4af87a63eca5fc352891726e099665e101
Denis Atkesone <denis.atkesone@gmail.com>
2018-02-06 14:21:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,7 +26,7 @@ class Student
   def self.first_X_students_in_grade_10(num)
     DB[:conn].execute("SELECT * FROM students WHERE grade='10th' LIMIT ?", num)
   end
-  
+
   def self.count_all_students_in_grade_9
     DB[:conn].execute("SELECT COUNT(*) FROM students WHERE grade='9th'")[0].flatten
   end
